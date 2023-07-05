c0f85f9217c77f5809b23cdcce5c70b025896c0f
Alonso Villegas <alonsovillegas816@gmail.com>
2023-07-05 12:11:40 -0500

    feat: add meta callback

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -34,3 +34,35 @@ export async function googleCallback(req: Request, res: Response) {
 
     res.redirect("https://mojito.online")
 }
+
+export async function metaCallback(req: Request, res: Response) {
+    const code = req.query["code"] as string
+    const state = req.query["state"] as string
+
+    console.log("meta callback")
+    console.log(code)
+    console.log(state)
+
+    const conn = await req.datasources.connection.getById(state)
+
+    if (!conn) {
+        return res.status(401).send("Invalid state token")
+    }
+
+    const authClient = await req.core.authFactory.createAndInit(conn.source, code)
+    const token = authClient.getToken()
+
+    if (!token) {
+        return res.status(500).send("Something went wrong")
+    }
+
+    await req.datasources.connection.update(conn._id, {
+        accessToken: token.accessToken,
+        refreshToken: token.refreshToken,
+        tokenExpiration: token.expiration,
+    })
+
+    await req.datasources.user.update(conn._id, {})
+
+    res.redirect("https://mojito.online")
+}
