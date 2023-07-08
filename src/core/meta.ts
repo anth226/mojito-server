7ce9b0954ad78bd4566e665aea6e19f788aec3f0
Alonso Villegas <alonsovillegas816@gmail.com>
2023-07-08 18:12:04 -0500

    feat: add meta api

--- src/core/meta.ts
+++ src/core/meta.ts
@@ -1,15 +1,83 @@
+import { AxiosRequestConfig } from "axios"
 import { OAuth2Client } from "./oauth2"
+import dayjs from "dayjs"
 
-const baseUrl = "https://graph.facebook.com/v17.0"
+const API_URL = "https://graph.facebook.com/v17.0"
 
-export class FacebookApi {
-    constructor(private client: OAuth2Client) {}
+type ApiResponse<T> = {
+    data: T
+    paging: {
+        cursors: {
+            before: string
+            after: string
+        }
+        next?: string
+    }
+}
 
-    async getUserProfile() {
-        const resp = await this.client.get(
-            "https://openidconnect.googleapis.com/v1/userinfo"
+type AdAccount = {
+    id: string
+    account_id: string
+    name: string
+    business_name: string
+    age: number
+}
+
+type Insight = {
+    cpm: string
+    ctr: string
+    spend: string
+    clicks: string
+}
+
+export class MetaApi {
+    constructor(private client: OAuth2Client) { }
+
+    async getAdAccounts(): Promise<Array<AdAccount>> {
+        return this.getPaginated<AdAccount>(`${API_URL}/me/adaccounts`, {
+            params: {
+                fields: "account_id,name,business_name,age",
+                limit: 50,
+            },
+        })
+    }
+
+    async getInsights(
+        adAccountId: string,
+        from: Date,
+        to: Date
+    ): Promise<Array<Insight>> {
+        const since = dayjs(from).format("YYYY-MM-DD")
+        const until = dayjs(to).format("YYYY-MM-DD")
+
+        return this.getPaginated<Insight>(
+            `${API_URL}/${adAccountId}/insights`,
+            {
+                params: {
+                    fields: "impressions,ad_id,account_name,clicks,cpm,ctr,spend,purchase_roas",
+                    time_increment: 1,
+                    time_range: `{"since":"${since}","until":"${until}"}`,
+                    action_attribution_windows: "1d_click,7d_click,1d_view",
+                    limit: 50,
+                },
+            }
         )
+    }
 
-        console.log(resp.data)
+    private async getPaginated<T>(
+        url: string,
+        config?: AxiosRequestConfig
+    ): Promise<Array<T>> {
+        let resp = await this.client
+            .axios()
+            .get<ApiResponse<Array<T>>>(url, config)
+        let objects = resp.data.data
+
+        while (resp.data.paging.next) {
+            resp = await this.client.axios().get(resp.data.paging.next)
+            objects = objects.concat(resp.data.data)
+        }
+
+        return objects
     }
 }
