6e203037103ffc1bff28c96c3a5dbf092625b7d4
endlessorhub <anthonyhutchi99@gmail.com>
2023-07-10 12:34:50 -0500

    fix: remove mock google query

--- src/core/google.ts
+++ src/core/google.ts
@@ -2,12 +2,4 @@ import { OAuth2Client } from "./oauth2"
 
 export class GoogleApi {
     constructor(private client: OAuth2Client) {}
-
-    async getUserProfile() {
-        const resp = await this.client.get(
-            "https://openidconnect.googleapis.com/v1/userinfo"
-        )
-
-        console.log(resp.data)
-    }
 }
